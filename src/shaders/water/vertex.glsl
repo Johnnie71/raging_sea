5680b6bf50a2a9a98ecccdd5c2099c1f3af538db
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-08-14 11:47:59 -0400

    added speed

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -1,12 +1,13 @@
 uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
 uniform float uTime;
+uniform float uBigWavesSpeed;
 
 void main() {
   vec4 modelPosition = modelMatrix *vec4(position, 1.0);
 
   // Elevation
-  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime) * sin(modelPosition.z * uBigWavesFrequency.y + uTime) * uBigWavesElevation;
+  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * uBigWavesElevation;
   modelPosition.y = elevation;
 
   vec4 viewPosition = viewMatrix * modelPosition;
